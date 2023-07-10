82b29cd3b5950758ee7f595beba7a0a095ff68f3
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-07-10 09:52:37 +0200

    Modifications to navbar so that links grow and change colour to indicate current page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { Logo } from '../assets/index.js'
 
+const activeClass = 'text-xl text-slate-400'
+
 function Header() {
   return (
-    <header className='grid grid-cols-2 md:grid-cols-[180px_1fr_180px] border-slate-100'>
+    <header className='grid grid-cols-2 md:grid-cols-[180px_1fr_180px] border-slate-100 shadow-lg'>
         <Link className='flex justify-center bg-slate-50  hover:bg-slate-200 ease-in-out duration-75' to='/'>
             <img src={Logo} alt="Histeria" width={150}/>
         </Link>
-        <Link to='/contact' className='border-2 border-slate-800 md:col-start-3 flex gap-2 items-center justify-center text-lg text-slate-200 bg-slate-900  hover:bg-slate-200 hover:text-xl hover:text-slate-900 ease-in-out duration-200 hover:border-slate-300 '>
+        <Link to='/contact' className='border-2 md:col-start-3 flex gap-2 items-center justify-center text-lg text-slate-200 bg-slate-900  hover:bg-slate-200 hover:text-xl hover:text-slate-900 ease-in-out duration-200'>
             <h2>Let's talk!</h2>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
@@ -17,17 +19,26 @@ function Header() {
         <nav className='col-span-2 bg-slate-50 md:row-start-1 md:col-start-2 md:col-end-3 flex items-center justify-center'>
             <ul className='flex gap-10 max-w-sm overflow-hidden snap-always text-lg'>
                 <li className='hover:text-xl ease-in-out duration-150'>
-                    <NavLink to='/'>
+                    <NavLink to='/'
+                        className={({ isActive }) => 
+                        isActive ? activeClass : undefined
+                    }>
                         HOME
                     </NavLink>
                 </li>
                 <li className='hover:text-xl ease-in-out duration-150'>
-                    <NavLink to='About'>
+                    <NavLink to='about'
+                        className={({ isActive }) => 
+                        isActive ? activeClass : undefined
+                    }>
                         ABOUT
                     </NavLink>
                 </li>
                 <li className='hover:text-xl ease-in-out duration-150'>
-                    <NavLink to='Projects'>
+                    <NavLink to='projects'
+                        className={({ isActive }) => 
+                        isActive ? activeClass : undefined
+                    }>
                         PROJECTS
                     </NavLink>
                 </li>
