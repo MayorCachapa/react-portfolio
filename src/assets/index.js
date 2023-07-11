3097ea5a0258efa99c9ae0752cb6fded98ce5d30
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-07-11 02:42:40 +0200

    Finished "Project" section. Added "Services" but pending to be finished and populated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,15 @@
 import Logo from './logo-no-background-2.png'
 import Banner from './banner.jpg'
 import PatternImg from './background-pattern.png'
-import Casaya1 from './casaya-2.jpg'
+import Casaya1 from './casaya-1.jpg'
 import Waxworks1 from './waxworks-1.jpg'
+import Histeria1 from './histeria-1.jpg'
 
 export { 
     Logo,
     Banner,
     PatternImg,
     Casaya1,
-    Waxworks1
+    Waxworks1,
+    Histeria1
 }
--- src/data.js
+++ src/data.js
@@ -1,26 +1,26 @@
-import { Casaya1, Waxworks1 } from './assets'
+import { Casaya1, Waxworks1, Histeria1 } from './assets'
 
 
 export const portfolioData = [
     {
-        title: 'Casaya',
-        thumbnail: Casaya1,
-        alt:'',
-        category:'',
+        title: 'Histeria Labs',
+        thumbnail: Histeria1,
+        alt:'Homepage of Histeria Labs',
+        category:'Frontend Development / portfolio',
         link: '#'
     },
     {
         title: 'WaxWorks',
         thumbnail: Waxworks1,
-        alt:'',
-        category:'',
+        alt:'Homepage of WaxWorks',
+        category:'Fullstack Development / e-commerce',
         link: '#'
     },
     {
         title: 'Casaya',
-        thumbnail: "",
-        alt:'',
-        category:'',
+        thumbnail: Casaya1,
+        alt:'Homepage of Casaya',
+        category:'Backend Development / e-commerce',
         link: '#'
     },
     {
